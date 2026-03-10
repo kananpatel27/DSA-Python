a19eddf0e65bfda524345300f262ae75a32e57d1
kananpatel27 <kananlpatel27@gmail.com>
2026-03-10 18:29:52 +0530

    Add files via upload

---
+++ Largest and Smallest in list.py
@@ -0,0 +1,13 @@
+n = int(input("Enter number of elements: ")) 
+print("n"), endl
+
+arr = []
+
+print("Enter elements: ")
+for i in range(n):
+  arr.append(int(input()))
+print(i), endl
+
+#Assume first element as largest and smallest 
+largest = arr[0]
+smallest = arr[0]
